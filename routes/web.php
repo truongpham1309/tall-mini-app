0659ae8690745b0f3e4305bdf6593cc303b8bcd6
Pham Dinh Truong <truong13092002@gmail.com>
2025-06-18 16:58:12 +0700

    ui mini-app --done

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,30 @@ use Illuminate\Support\Facades\Route;
 |_________________________________________________________________________|
 */
 
+// Route::get('/', function () {
+//     return view('home.page');
+// });
+
+// Route::get('/about-us', function () {
+//     return view('about-us.page');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact.page');
+// });
+
+// Route::get('/services', function () {
+//     return view('services.page');
+// });
+
+// Route::get('/faq', function () {
+//     return view('faq.page');
+// });
+
+// Route::get('/hyra-network', [HomeHyraNetworkController::class, 'index']);
 Route::get('/', function () {
-    return view('home.page');
-});
-
-Route::get('/about-us', function () {
-    return view('about-us.page');
-});
-
-Route::get('/contact', function () {
-    return view('contact.page');
-});
-
-Route::get('/services', function () {
-    return view('services.page');
-});
-
-Route::get('/faq', function () {
-    return view('faq.page');
-});
-
-Route::get('/hyra-network', [HomeHyraNetworkController::class, 'index']);
-Route::get('/mini-app', function () {
     return view('mini-app.home.page');
-})->name('mini-app.home');;
+})->name('mini-app.home');
 
 Route::get('/mini-app/categories', function () {
     return view('mini-app.categories.page');
